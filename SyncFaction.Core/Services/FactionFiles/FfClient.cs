c3e1a8f428a91b04bb4b9fecb89cc4ec98f25360
Rostislav Kirillov <rast@tochka.com>
2022-10-01 17:18:53 +0400

    fix: community patch updates should work now

--- SyncFaction.Core/Services/FactionFiles/FfClient.cs
+++ SyncFaction.Core/Services/FactionFiles/FfClient.cs
@@ -267,6 +267,8 @@ public class FfClient
                 result.Add(id.Value);
                 log.LogInformation($"Community update {i} id: **{id}**");
             }
+
+            i++;
         } while (id != null);
 
         return result;
