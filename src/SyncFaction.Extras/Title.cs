f08f8b56df8407547926f8207262bf4093f1e2e1
Rostislav Kirillov <rast@tochka.com>
2023-06-24 16:35:56 +0400

    feat: restored generated build time

--- src/SyncFaction.Extras/Title.cs
+++ src/SyncFaction.Extras/Title.cs
@@ -3,7 +3,6 @@ namespace SyncFaction.Extras;
 public static class Title
 {
     public static readonly string AppName = "SyncFaction";
-    //public static readonly string Version = GeneratedBuildTime.GetValue();
-    public static readonly string Version = "todo_version";
+    public static readonly string Version = GeneratedBuildTime.GetValue();
     public static readonly string Value = $"{AppName} (build {Version})";
 }
