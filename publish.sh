99817b3d354d02151037302cbbb8b89fc22ab040
Rostislav Kirillov <rast@tochka.com>
2023-07-10 04:29:07 +0400

    feat: implementing TODOs

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 # /bin/bash -ex
-rm -rf publish
+rm -rf _publish
 dotnet clean src
 dotnet test src || exit 1
-dotnet publish src/SyncFaction -o publish
-dotnet publish src/SyncFaction.Toolbox -o publish
+dotnet publish src/SyncFaction -o _publish
+dotnet publish src/SyncFaction.Toolbox -o _publish
